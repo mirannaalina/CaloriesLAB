64f3bc7ab6c28a7466ae2d072f0dedd2dac9725d
mirannaalina <v_mxa@yahoo.com>
2020-05-12 11:19:35 +0300

    create file new

--- src/main/java/com/calories/caloriesproject/controller/FoodController.java
+++ src/main/java/com/calories/caloriesproject/controller/FoodController.java
@@ -1,10 +1,11 @@
 package com.calories.caloriesproject.controller;
 
-import com.calories.caloriesproject.config.PdfGeneratorUtil;
+
 import com.calories.caloriesproject.persistence.Food;
 import com.calories.caloriesproject.persistence.FoodRepository;
 import com.calories.caloriesproject.service.FoodService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -49,20 +57,21 @@ public class FoodController {
 
     }
 
+    @GetMapping("/download")
+    public void downloadfile(HttpServletRequest request,
+                                           HttpServletResponse response){
+       String dataDirectory = request.getServletContext().getRealPath("/Users/mirannaalina/Downloads");
+       Path file = Paths.get(dataDirectory);
+       if(Files.exists(file))
+           response.setContentType("application/pdf");
+       response.addHeader("Content-Disposition","attachment");
+       try{
+           Files.copy(file,response.getOutputStream());
+           response.getOutputStream().flush();
+       }catch(IOException e){
+           e.printStackTrace();
+        }
 
-    @Autowired
-    public PdfGeneratorUtil pdfGeneratorUtil;
-
-    @GetMapping("/html2pdf")
-    public String html2pdf(){
-        Map<String,String> data = new HashMap<>();
-        data.put("te222","fail");
-                try {
-                    pdfGeneratorUtil.createPdf("index", data);
-                }catch (Exception e){
-                    System.out.println("failed");
-                }
-                return "redirect:/home";
     }
 
 }
